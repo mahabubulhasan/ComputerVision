fb16a788dc8d602a3e4d0ae34fa47721194c7cfd
Mahabubul Hasan <codehasan@gmail.com>
2025-09-07 20:16:38 +0600

    Basic operations with open cv

---
+++ puppy.py
@@ -0,0 +1,12 @@
+import cv2
+
+img = cv2.imread('DATA/00-puppy.jpg')
+
+while True:
+    cv2.imshow('Puppy', img)
+
+    # 27 is for Esc key
+    if(cv2.waitKey(1) & 0xFF == 27):
+        break
+
+cv2.destroyAllWindows()
